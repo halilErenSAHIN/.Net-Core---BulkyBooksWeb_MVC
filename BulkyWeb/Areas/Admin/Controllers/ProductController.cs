064911a59e9e2fe8bbace1d00c39fde2419c518d
Halil Eren Şahin <halilerensahin@gmail.com>
2024-09-01 20:01:00 +0300

    Create and Edit product controllers and views are deleted and created a upsert controller and view to tkae place for both create and update

--- BulkyWeb/Areas/Admin/Controllers/ProductController.cs
+++ BulkyWeb/Areas/Admin/Controllers/ProductController.cs
@@ -23,7 +23,7 @@ namespace BulkyBookWeb.Areas.Admin.Controllers
         }
 
         //---Create Controller---Halil Eren Şahin
-        public IActionResult Create()
+        public IActionResult Upsert(int? id) //upload + insert = Upsert
         {
 
             ProductVM productVM = new()
@@ -35,12 +35,24 @@ namespace BulkyBookWeb.Areas.Admin.Controllers
                 }),
                 Product = new Product()
             };
-            return View(productVM);
+            if (id == null || id == 0)
+            {
+                //Create
+                return View(productVM);
+            }
+            else
+            {
+                //Update
+                productVM.Product = _unitOfWork.Product.Get(u => u.Id == id);
+                return View(productVM);
+
+            }
+
 
         }
 
         [HttpPost]
-        public IActionResult Create(ProductVM productVM)
+        public IActionResult Upsert(ProductVM productVM, IFormFile? file)
         {
 
             if (ModelState.IsValid)
@@ -64,41 +76,6 @@ namespace BulkyBookWeb.Areas.Admin.Controllers
         }
 
 
-
-        //-------Edit Controller-----Halil Eren Şahin
-
-        public IActionResult Edit(int? id)
-        {
-
-            if (id == null || id == 0)
-            {
-                return NotFound();
-            }
-            Product? productFromDb = _unitOfWork.Product.Get(u => u.Id == id);
-            if (productFromDb == null)
-            {
-                return NotFound();
-            }
-            return View(productFromDb);
-
-        }
-
-        [HttpPost]
-        public IActionResult Edit(Product obj)
-        {
-
-            if (ModelState.IsValid)
-            {
-                _unitOfWork.Product.Update(obj);
-                _unitOfWork.Save();
-                TempData["success"] = "Product updated successfully";
-                return RedirectToAction("Index");
-            }
-            return View();
-
-
-
-        }
 
         //-------Delete Controller-----Halil Eren Şahin
 
